0ef1ae97b7698f00c303acce99ee21332a5ebad4
sani2608 <sanikumar.sahani@gmail.comdsfasdfa>
2022-06-21 16:26:44 +0530

    property list

---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,57 @@
+import "./PropertyList.css";
+
+const PropertyList = () => {
+  return (
+    <div className="pList">
+      <div className="pListItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/city/540x270/684727.webp?k=a6fd59b0c31df52ae2a907dcf2441c7842bb56e2e68d89bffd2398024c0812d3&o="
+          alt="vs"
+          className="pListImg"
+        />
+        <div className="pListTitle">
+          <h1>Agra</h1>
+          <h2>23 properties</h2>
+        </div>
+      </div>
+
+      <div className="pListItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/city/540x270/684727.webp?k=a6fd59b0c31df52ae2a907dcf2441c7842bb56e2e68d89bffd2398024c0812d3&o="
+          alt="vs"
+          className="pListImg"
+        />
+        <div className="pListTitle">
+          <h1>Agra</h1>
+          <h2>23 properties</h2>
+        </div>
+      </div>
+
+      <div className="pListItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/city/540x270/684727.webp?k=a6fd59b0c31df52ae2a907dcf2441c7842bb56e2e68d89bffd2398024c0812d3&o="
+          alt="vs"
+          className="pListImg"
+        />
+        <div className="pListTitle">
+          <h1>Agra</h1>
+          <h2>23 properties</h2>
+        </div>
+      </div>
+
+      <div className="pListItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/city/540x270/684727.webp?k=a6fd59b0c31df52ae2a907dcf2441c7842bb56e2e68d89bffd2398024c0812d3&o="
+          alt="vs"
+          className="pListImg"
+        />
+        <div className="pListTitle">
+          <h1>Agra</h1>
+          <h2>23 properties</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyList;
