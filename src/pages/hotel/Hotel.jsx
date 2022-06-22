ab392bce28b744eff2d73033e2d06a18f37c09f0
sani2608 <sanikumarsahani@gmail.com>
2022-06-22 22:07:44 +0530

    image slider

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,7 +1,15 @@
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircleArrowLeft,
+  faCircleArrowRight,
+  faCircleXmark,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Footer from "components/footer/Footer";
 import Header from "components/header/Header";
+import MailList from "components/mailList/MailList";
 import Navbar from "components/Navbar/Navbar";
+import { useState } from "react";
 import "./Hotel.css";
 
 const Hotel = () => {
@@ -25,60 +33,97 @@ const Hotel = () => {
       src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1",
     },
   ];
+
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [open, setOpen] = useState(false);
+
   const handleOpen = (i) => {
-    console.log(i);
+    setSlideNumber(i);
+    setOpen(true);
   };
+
+  const handleMove = (direction) => {
+    let newSlideNumber;
+
+    if (direction === "l") {
+      newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
+    } else {
+      newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
+    }
+
+    setSlideNumber(newSlideNumber);
+  };
+
   return (
     <div>
       <Navbar />
-      <Header type="list" />
-      <div className="hotelContainer">
-        <div className="hotelWrapper">
-          <h1 className="hotelTitle">Grand Hotel</h1>
-          <div className="hotelAddress">
+      <Header type='list' />
+      <div className='hotelContainer'>
+        {open && (
+          <div className='slider'>
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              className='close'
+              onClick={() => setOpen(false)}
+            />
+            <FontAwesomeIcon
+              icon={faCircleArrowLeft}
+              className='arrow'
+              onClick={() => handleMove("l")}
+            />
+            <div className='sliderWrapper'>
+              <img src={photos[slideNumber].src} alt='' className='sliderImg' />
+            </div>
+            <FontAwesomeIcon
+              icon={faCircleArrowRight}
+              className='arrow'
+              onClick={() => handleMove("r")}
+            />
+          </div>
+        )}
+        <div className='hotelWrapper'>
+          <button className='bookNow'>Reserve or Book Now!</button>
+          <h1 className='hotelTitle'>Grand Hotel</h1>
+          <div className='hotelAddress'>
             <FontAwesomeIcon icon={faLocationDot} />
             <span>Elton St 125 New york</span>
           </div>
-          <span className="hotelDistance">
-            Excellent location - 500 from center
-          </span>
-          <span className="hotelPriceHighlight">
+          <span className='hotelDistance'>Excellent location - 500 from center</span>
+          <span className='hotelPriceHighlight'>
             Book a stay over $114 at this property and get a free airport taxi
           </span>
-          <div className="hotelImages">
+          <div className='hotelImages'>
             {photos.map((photo, i) => (
-              <div className="hotelImgWrapper" key={i}>
+              <div className='hotelImgWrapper' key={i}>
                 <img
                   onClick={() => handleOpen(i)}
                   src={photo.src}
-                  alt=""
-                  className="hotelImg"
+                  alt=''
+                  className='hotelImg'
                 />
               </div>
             ))}
           </div>
-          <div className="hotelDetails">
-            <div className="hotelDetailsTexts">
-              <h1 className="hotelTitle">Stay in the heart of City</h1>
-              <p className="hotelDesc">
-                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
-                Street Apartments has accommodations with air conditioning and
-                free WiFi. The units come with hardwood floors and feature a
-                fully equipped kitchenette with a microwave, a flat-screen TV,
-                and a private bathroom with shower and a hairdryer. A fridge is
-                also offered, as well as an electric tea pot and a coffee
-                machine. Popular points of interest near the apartment include
-                Cloth Hall, Main Market Square and Town Hall Tower. The nearest
-                airport is John Paul II International Kraków–Balice, 16.1 km
-                from Tower Street Apartments, and the property offers a paid
-                airport shuttle service.
+          <div className='hotelDetails'>
+            <div className='hotelDetailsTexts'>
+              <h1 className='hotelTitle'>Stay in the heart of City</h1>
+              <p className='hotelDesc'>
+                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower Street
+                Apartments has accommodations with air conditioning and free WiFi. The
+                units come with hardwood floors and feature a fully equipped kitchenette
+                with a microwave, a flat-screen TV, and a private bathroom with shower and
+                a hairdryer. A fridge is also offered, as well as an electric tea pot and
+                a coffee machine. Popular points of interest near the apartment include
+                Cloth Hall, Main Market Square and Town Hall Tower. The nearest airport is
+                John Paul II International Kraków–Balice, 16.1 km from Tower Street
+                Apartments, and the property offers a paid airport shuttle service.
               </p>
             </div>
-            <div className="hotelDetailsPrice">
+            <div className='hotelDetailsPrice'>
               <h1>Perfect for a 9-night stay!</h1>
               <span>
-                Located in the real heart of Krakow, this property has an
-                excellent location score of 9.8!
+                Located in the real heart of Krakow, this property has an excellent
+                location score of 9.8!
               </span>
               <h2>
                 <b>$945</b> (9 nights)
@@ -87,6 +132,8 @@ const Hotel = () => {
             </div>
           </div>
         </div>
+        <MailList />
+        <Footer />
       </div>
     </div>
   );
